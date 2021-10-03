3aa9f51351aa742ec9d78c229d473e1f4c93341b
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-10-02 23:24:54 -0300

    commit 2

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+export const firebaseConfig = {
+
+    apiKey: "AIzaSyAa_twzUhzGCff0thTzYQGEF1uCmqaUTSs",
+  
+    authDomain: "desafio-auth-g9.firebaseapp.com",
+  
+    projectId: "desafio-auth-g9",
+  
+    storageBucket: "desafio-auth-g9.appspot.com",
+  
+    messagingSenderId: "781688285907",
+  
+    appId: "1:781688285907:web:a2cde406a9af1718b90226"
+  
+  };
+  
--- src/main.js
+++ src/main.js
@@ -3,7 +3,15 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
+import Firebase from "firebase"
+import { firebaseConfig } from "../firebase-config"
+
+Firebase.initializeApp(firebaseConfig);
+
 Vue.config.productionTip = false
+
+
 
 new Vue({
   router,
