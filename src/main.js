216457a2ecaba20ac62c507e2a9d2e9560f54bdf
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-10-03 16:29:27 -0300

    commit 6

--- src/main.js
+++ src/main.js
@@ -2,16 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import Element from 'element-ui'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "bootstrap/dist/js/bootstrap.bundle.js";
 
 
 import Firebase from "firebase"
 import { firebaseConfig } from "../firebase-config"
+import { Message } from "element-ui" 
 
 Firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
+Vue.use(Element)
 
 
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,21 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Store from '../store';
 import Login from '../views/Login.vue'
+import Firebase from "firebase"
 
 Vue.use(VueRouter)
 
 const routes = [
   {
-    path: '/',
+    path: "*",
+    redirect: "/home"
+  },
+  {
+    path: '/home',
     name: 'Home',
     component: Home,
     meta: {
-      login:false,
+      login: true,
     }
   },
   {
@@ -35,5 +40,17 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 })
+
+//router.beforeEach((to, from, next) => {
+//  let user = Firebase.auth().currentUser;
+//  let authRequired = to.matched.some(rpute => router.meta.login);
+//  if (!user && authRequired) {
+//    next('login');
+//  } else if (user && !authRequired) {
+//    next('home');
+//  } else {
+//    next();
+//  }
+//})
 
 export default router
